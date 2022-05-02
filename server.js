18a6f9f070851d876b5ab3b3ee5982ce652e9def
Charles K.S <ctmbr@protonmail.com>
2022-05-02 10:35:00 -0700

    Adding noteWriter.js

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const express = require('express');
 const PORT = 3001;
 const app = express();
 const htmlRoutes = require('./routes/html_routes')
-// const apiRoutes = require('./routes/api_routes')
+const apiRoutes = require('./routes/api_routes')
+
+app.use('/api', apiRoutes)
 app.use('/', htmlRoutes)
 
 
