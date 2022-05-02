2cee89d5879baadc7d1148924cf29ef3374dc63c
Charles K.S <ctmbr@protonmail.com>
2022-05-02 10:30:49 -0700

    Adding html routes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const PORT = 3001;
 const app = express();
 const htmlRoutes = require('./routes/html_routes')
 // const apiRoutes = require('./routes/api_routes')
+app.use('/', htmlRoutes)
 
 
 app.use(express.json());
