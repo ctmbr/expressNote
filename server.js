e1c3b606e40f09eda891166f2e38536126ed422c
Charles K.S <ctmbr@protonmail.com>
2022-05-02 10:19:11 -0700

    Adding Route file structure

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const PORT = 3001;
 const app = express();
+const htmlRoutes = require('./routes/html_routes')
+// const apiRoutes = require('./routes/api_routes')
 
 
 app.use(express.json());
