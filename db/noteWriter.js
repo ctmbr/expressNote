80f0be0588751f0d86e63b03e18f7842943ccd3d
Charles K.S <ctmbr@protonmail.com>
2022-05-06 10:33:36 -0700

    Adding delete route and method

--- db/noteWriter.js
+++ db/noteWriter.js
@@ -1,29 +1,54 @@
-const fs = require('fs')
-const util = require('util')
-const { v4 } = require('uuid')
+const fs = require("fs");
+const util = require("util");
+const { v4 } = require("uuid");
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
 class NoteWriter {
-    read() { return readFileAsync("./db/db.json", "utf-8") }
-    write(note) { return writeFileAsync("./db/db.json", JSON.stringify(note)) }
+    read() {
+        return readFileAsync("./db/db.json", "utf-8");
+    }
+    write(note) {
+        return writeFileAsync("./db/db.json", JSON.stringify(note));
+    }
     getNotes() {
         return this.read().then((allNotes) => {
-            let parsedNotes
+            let parsedNotes;
             try {
-                parsedNotes = [].concat(JSON.parse(allNotes))
+                parsedNotes = [].concat(JSON.parse(allNotes));
             } catch (err) {
-                parsedNotes = []
+                parsedNotes = [];
             }
-            return parsedNotes
-        })
+            return parsedNotes;
+        });
     }
     writeNote(note) {
-        if (!note.title || !note.text) { throw new Error("Empty Note") }
-        note.id = v4()
-        return this.getNotes().then((notes) => { return notes.push(note) }).then((notes) => { return this.write(notes) }).then(() => { return note })
+        if (!note.title || !note.text) {
+            throw new Error("Empty Note");
+        }
+        note.id = v4();
+        console.log(note);
+        return this.getNotes()
+            .then((notes) => {
+                return [...notes, note];
+            })
+            .then((notes) => {
+                return this.write(notes);
+            })
+            .then(() => {
+                return note;
+            });
+    }
+    deleteNote(id) {
+        return this.getNotes()
+            .then((notes) => {
+                return notes.filter((note) => note.id !== id);
+            })
+            .then((toKeep) => {
+                return this.write(toKeep);
+            });
     }
 }
 
-module.exports = new NoteWriter()
+module.exports = new NoteWriter();
--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,10 +1,34 @@
-const router = require('express').Router()
-const noteWriter = require('../db/noteWriter')
+const router = require("express").Router();
+const noteWriter = require("../db/noteWriter");
 
 router.get("/notes", (req, res) => {
-    noteWriter.getNotes().then((notes) => { return res.json(notes) }).catch((err) => { res.status(500).json(err) })
-})
+    noteWriter
+        .getNotes()
+        .then((notes) => {
+            return res.json(notes);
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
+});
 router.post("/notes", (req, res) => {
-    noteWriter.writeNote(req.body).then(note => { return res.json(note) }).catch((err) => { res.status(500).json(err) })
-})
-module.exports = router
+    noteWriter
+        .writeNote(req.body)
+        .then((note) => {
+            return res.json(note);
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
+});
+router.delete("/notes/:id", (req, res) => {
+    noteWriter
+        .deleteNote(req.params.id)
+        .then(() => {
+            res.json({ success: true });
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
+});
+module.exports = router;
